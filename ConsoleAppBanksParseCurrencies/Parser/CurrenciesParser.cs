e92609afdaddb37a39780538f0bf9bae1f950ec1
MirrorV74 <valera_sinitsa@bk.ru>
2023-07-15 12:56:33 +0300

    Parsing done

--- ConsoleAppBanksParseCurrencies/Parser/CurrenciesParser.cs
+++ ConsoleAppBanksParseCurrencies/Parser/CurrenciesParser.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using ConsoleAppBanksParseCurrencies.Models;
 using HtmlAgilityPack;
 
@@ -5,26 +6,51 @@ namespace ConsoleAppBanksParseCurrencies.Parser;
 
 public class CurrenciesParser
 {
-    private const string url = "https://mainfin.ru/currency";
-    private const string xPathExpression = "//tr[@class='row body tr-turn odd']";
-    private HtmlWeb htmlWeb;
+    private const string _url = "https://mainfin.ru/currency";
+    private const string _xPathExpressionEven = "//tr[@class=' row body tr-turn even']";
+    private const string _xPathExpressionOdd = "//tr[@class=' row body tr-turn odd']";
+    private HtmlWeb _htmlWeb;
 
     public CurrenciesParser()
     {
-        htmlWeb = new HtmlWeb();
-        htmlWeb.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+        _htmlWeb = new HtmlWeb();
+        _htmlWeb.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+    }
+
+    private List<Currency> ParseNodes(HtmlNodeCollection nodes)
+    {
+        List<Currency> currencies = new List<Currency>();
+
+        foreach (HtmlNode htmlNode in nodes)
+        {
+            string bankName = htmlNode.ChildNodes[0].InnerText;
+            decimal usdBankBuy = decimal.Parse(htmlNode.ChildNodes[1].InnerText, new CultureInfo("en-US"));
+            decimal usdBankSell = decimal.Parse(htmlNode.ChildNodes[2].InnerText, new CultureInfo("en-US"));
+
+            currencies.Add(new Currency()
+            {
+                BankName = bankName,
+                UsdBankBuy = usdBankBuy,
+                UsdBankSell = usdBankSell
+            });
+        }
+
+        return currencies;
     }
 
     public List<Currency> LoadAllCurrencies()
     {
-        HtmlDocument document = htmlWeb.Load(url);
+        HtmlDocument document = _htmlWeb.Load(_url);
 
-        //foreach (HtmlNode htmlNode in document.DocumentNode.SelectNodes(xPathExpression))
-        //{
-        //    
-        //}
-        var a = document.DocumentNode.SelectNodes(xPathExpression);
-        
-        return null;
+        HtmlNodeCollection nodesEven = document.DocumentNode.SelectNodes(_xPathExpressionEven);
+
+        HtmlNodeCollection nodesOdd = document.DocumentNode.SelectNodes(_xPathExpressionOdd);
+
+        List<Currency> currencies = new List<Currency>();
+
+        currencies.AddRange(ParseNodes(nodesEven));
+        currencies.AddRange(ParseNodes(nodesOdd));
+
+        return currencies;
     }
 }
